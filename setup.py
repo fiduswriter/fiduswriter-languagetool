2167471b0318f1b2c6121f950ce30de0d67d1ec4
Johannes Wilm <mail@johanneswilm.org>
2019-09-22 14:39:48 +0200

    3.7.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='fiduswriter-languagetool',
-    version='3.7.0rc1',
+    version='3.7.0',
     packages=find_namespace_packages(),
     include_package_data=True,
     license='AGPL License',
