74f797ab5c304dc4e86082cf35afa6195fe45065
Johannes Wilm <mail@johanneswilm.org>
2020-11-12 08:48:25 +0100

    3.9.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='fiduswriter-languagetool',
-    version='3.9.0',
+    version='3.9.1',
     packages=find_namespace_packages(),
     include_package_data=True,
     exclude_package_data={
