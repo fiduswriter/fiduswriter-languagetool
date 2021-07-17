fca5acfee10d7cab0f7f9ed9b806a955f49e5ece
Johannes Wilm <mail@johanneswilm.org>
2021-07-17 16:58:26 +0200

    3.10.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='fiduswriter-languagetool',
-    version='3.9.1',
+    version='3.10.0',
     packages=find_namespace_packages(),
     include_package_data=True,
     exclude_package_data={
@@ -45,7 +45,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
