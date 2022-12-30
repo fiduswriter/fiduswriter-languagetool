d20e839a30a9841d4ebe06a4f0c2abb6f5a71c8a
Johannes Wilm <mail@johanneswilm.org>
2022-12-30 09:45:29 +0100

    3.11.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="fiduswriter-languagetool",
-    version="3.11.0",
+    version="3.11.1",
     packages=find_namespace_packages(),
     include_package_data=True,
     exclude_package_data={"": ["configuration.py", "django-admin.py", "build/*"]},
