ee7215cd5a400b05714a3d7d7ded1bddd7105aab
Johannes Wilm <mail@johanneswilm.org>
2023-03-01 07:49:15 +0100

    setup.py => pyproject.toml

--- README.md
+++ README.md
@@ -5,29 +5,37 @@ FidusWriter-Languagetool
 FidusWriter-Languagetool is a Fidus writer plugin to connect a Fidus Writer instance
 with Languagetool (LT).
 
-**NOTE:** You should first install Languagetool (a Java program) and make it run as a server. It is included in the Ubuntu Snap version of Fidus Writer so that will be the simplest way to install it for 99% of users. 
+**NOTE:** You should first install Languagetool (a Java program) and make it run as a server. It is included in the Ubuntu Snap version of Fidus Writer so that will be the simplest way to install it for 99% of users.
 
 Installation
 ------------
 
 1. Install Fidus Writer with the correct version of the plugin like this:
 
-    pip install fiduswriter[languagetool]
+```
+pip install fiduswriter[languagetool]
+```
 
 2. Add "languagetool" to your INSTALLED_APPS setting in the configuration.py file
    like this::
 
-    INSTALLED_APPS += (
-        ...
-        'languagetool',
-    )
+```python
+INSTALLED_APPS += (
+    ...
+    'languagetool',
+)
+```
 
 3. Add a setting for the URL where you are running Languagetool in the configuration.py file like this:
 
-    LT_URL = 'http://localhost:8081'
+```python
+LT_URL = 'http://localhost:8081'
+```
 
 4. Create the needed JavaScript files by running this::
 
-    python manage.py transpile
+```
+python manage.py transpile
+```
 
 5. (Re)start your Fidus Writer server.
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from glob import glob
 from setuptools import find_namespace_packages, setup
 from setuptools.command.build_py import build_py as _build_py
 
+
 # From https://github.com/pypa/setuptools/pull/1574
 class build_py(_build_py):
     def find_package_modules(self, package, package_dir):
@@ -24,34 +25,11 @@ class build_py(_build_py):
         return modules
 
 
-with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
-    README = readme.read()
-
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="fiduswriter-languagetool",
-    version="3.11.1",
     packages=find_namespace_packages(),
     include_package_data=True,
     exclude_package_data={"": ["configuration.py", "django-admin.py", "build/*"]},
-    license="AGPL License",
-    description="A Fidus Writer plugin to connect to Languagetool.",
-    long_description=README,
-    url="https://github.com/fiduswriter/fiduswriter-languagetool",
-    author="Johannes Wilm",
-    author_email="johannes@fiduswriter.org",
-    classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Framework :: Django :: 4.1",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-    ],
     cmdclass={"build_py": build_py},
 )
